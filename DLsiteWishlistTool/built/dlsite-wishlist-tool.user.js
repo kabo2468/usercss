5d29d507d46edd57b371516c638d5fd3e71b9721
kabo2468 <28654659+kabo2468@users.noreply.github.com>
2021-06-13 04:04:46 +0900

    [dlwtujs] Change match

--- DLsiteWishlistTool/built/dlsite-wishlist-tool.user.js
+++ DLsiteWishlistTool/built/dlsite-wishlist-tool.user.js
@@ -4,12 +4,12 @@
 // @author      kabo2468
 // @description Tool for DLsite Wishlist
 // @supportURL  https://github.com/kabo2468/user-css-script/issues
-// @match       https://www.dlsite.com/*/mypage/wishlist/*
+// @match       https://www.dlsite.com/*/mypage/wishlist*
 // @namespace   kabo2468.dlwtujs
 // @grant       none
 // @require     none
-// @downloadURL https://raw.githubusercontent.com/kabo2468/user-css-script/master/DLsiteWishlistTool/dlsite-wishlist-tool.user.js
-// @updateURL   https://raw.githubusercontent.com/kabo2468/user-css-script/master/DLsiteWishlistTool/dlsite-wishlist-tool.user.js
+// @downloadURL https://github.com/kabo2468/user-css-script/raw/master/DLsiteWishlistTool/built/dlsite-wishlist-tool.user.js
+// @updateURL   https://github.com/kabo2468/user-css-script/raw/master/DLsiteWishlistTool/built/dlsite-wishlist-tool.user.js
 // @homepageURL https://github.com/kabo2468/user-css-script/tree/master/DLsiteWishlistTool
 // ==/UserScript==
 
