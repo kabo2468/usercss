7a79a2bd45c30aa1177fb26fcec10558bc2810b5
kabo2468 <28654659+kabo2468@users.noreply.github.com>
2021-06-13 03:59:08 +0900

    [dlwtujs] build

---
+++ DLsiteWishlistTool/built/dlsite-wishlist-tool.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        DLsite Wishlist Tool
+// @version     1.0.0
+// @author      kabo2468
+// @description Tool for DLsite Wishlist
+// @supportURL  https://github.com/kabo2468/user-css-script/issues
+// @match       https://www.dlsite.com/*/mypage/wishlist/*
+// @namespace   kabo2468.dlwtujs
+// @grant       none
+// @require     none
+// @downloadURL https://raw.githubusercontent.com/kabo2468/user-css-script/master/DLsiteWishlistTool/dlsite-wishlist-tool.user.js
+// @updateURL   https://raw.githubusercontent.com/kabo2468/user-css-script/master/DLsiteWishlistTool/dlsite-wishlist-tool.user.js
+// @homepageURL https://github.com/kabo2468/user-css-script/tree/master/DLsiteWishlistTool
+// ==/UserScript==
